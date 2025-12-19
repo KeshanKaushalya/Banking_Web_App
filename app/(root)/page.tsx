9e3eab31583e6814fc0d6f5b5b07d03d3806090c
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-20 00:47:31 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Keshan'};
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+          <header className='home-header'>
+            <HeaderBox 
+              type="greeting"
+              title="Welcome"
+              user={loggedIn?.firstName || 'Guest'}
+              subtext="Access and manage your account and transaction efficiently."
+            />
+          </header>
+      </div>
+    </section>
   )
 }
 
