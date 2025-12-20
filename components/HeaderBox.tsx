b0e8cec9ab84b6b010144e2eb46dbc4dfdd5e9ea
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-20 14:47:07 +0530

    Update HeaderBox.tsx

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -1,7 +1,13 @@
 const HeaderBox = ({ type = "title", title, subtext, user}: HeaderBoxProps) => {
   return (
-    <div>
-      HeaderBox
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === 'greeting' && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+        </h1>
+        <p className="header-box-subtext">{subtext}</p>
     </div>
   )
 }
