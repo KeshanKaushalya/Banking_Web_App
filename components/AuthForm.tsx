720414416b1432f93b411dce13e8080f83289ca8
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-22 02:22:31 +0530

    Signup function completed

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -84,7 +84,7 @@ const AuthForm = ({ type }: { type: string }) => {
 
         <div className="flex flex-col gap-1 md:gap-3">
           <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
-            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+            {user ? "Account Create Successfully" : type === "sign-in" ? "Sign In" : "Sign Up"}
             <p className="text-16 font-normal text-gray-600">
               {user
                 ? "Link your account to get started"
