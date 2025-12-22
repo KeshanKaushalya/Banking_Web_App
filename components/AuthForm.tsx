c5709061e123e6cbb8cdfbcaa2034f5651d46ff0
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-22 23:30:14 +0530

    Update AuthForm.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -53,15 +53,15 @@ const AuthForm = ({ type }: { type: string }) => {
         }
 
         if(type === 'sign-in'){
-            // const response = await signIn({
-            //     email: data.email,
-            //     password: data.password
-            // })
+            const response = await signIn({
+                email: data.email,
+                password: data.password
+            })
 
-            // if(response) router.push('/')
+            if(response) router.push('/')
         }
     } catch (error) {
-        console.log(error);
+        console.log(error)
     } finally{
         setIsLoading(false);
     }
