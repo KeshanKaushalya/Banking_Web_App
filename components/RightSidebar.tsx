595da441c1be211a4c5510f5c73b3ea39737fa5a
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-23 16:18:27 +0530

    Update RightSidebar.tsx

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -14,10 +14,10 @@ const RightSidebar = ({ user, transactions, banks}: RightSidebarProps) => {
                 </div>
              <div className='profile-details'>
                <h1 className='profile-name'>
-                {user?.name || 'User'}
+                {user.name}
                </h1>
                <p className='profile-email'>
-                {user?.email || 'No email'}
+                {user.email}
                </p>
              </div>
             </div>
