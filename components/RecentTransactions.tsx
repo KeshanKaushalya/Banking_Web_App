8ac314b7290552c3bcefbb148c9a2af2b0bf80c3
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-29 00:52:51 +0530

    Update RecentTransactions.tsx

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { BankTabItem } from "./BankTabItem";
 import BankInfo from "./BankInfo";
+import TransactionsTable from "./TransactionsTable";
 
 const RecentTransactions = ({
   accounts,
@@ -45,6 +46,9 @@ const RecentTransactions = ({
               appwriteItemId={appwriteItemId}
               type="full"
             />
+
+            <TransactionsTable transactions={transactions} />
+            
 
             
           </TabsContent>
