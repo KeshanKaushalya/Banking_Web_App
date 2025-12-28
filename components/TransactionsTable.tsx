bdf6d7d30e92f5c067ae0db5920bf77b82effc0d
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-29 00:52:44 +0530

    Create TransactionsTable.tsx

---
+++ components/TransactionsTable.tsx
@@ -0,0 +1,37 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const TransactionsTable = () => {
+  return (
+    <Table>
+      <TableCaption className="text-indigo-500">Wealthix transforms everyday transactions into intelligent money management experiences.</TableCaption>
+      <TableHeader className="bg-[#f9fafb]">
+        <TableRow>
+          <TableHead className="px-2">Transaction</TableHead>
+          <TableHead className="px-2">Amount</TableHead>
+          <TableHead className="px-2">Status</TableHead>
+          <TableHead className="px-2">Date</TableHead>
+          <TableHead className="px-2 max-md:hidden">Channel</TableHead>
+          <TableHead className="px-2 max-md:hidden">Category</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
+
+export default TransactionsTable;
