82302d378cc286d2c92bb1cd012207feaada7047
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-29 00:22:53 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = async ({ searchParams: { id, page} }: SearchParamProps) => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({ 
     userId: loggedIn.$id 
@@ -37,7 +39,12 @@ const Home = async ({ searchParams: { id, page} }: SearchParamProps) => {
             />
           </header>
        
-       RECENT TRANSACTIONS
+       <RecentTransactions 
+         accounts={accountsData}
+         transactions={account?.transactions}
+         appwriteItemId={appwriteItemId}
+         page={currentPage}
+       />
       </div>
 
      <RightSidebar
