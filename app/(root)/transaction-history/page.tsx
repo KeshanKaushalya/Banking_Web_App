10d80626707b3d0a58a5cbc5b7c48f02acf6ad09
Keshan Kaushalya <madusankakeshan782@gmail.com>
2026-01-05 01:19:07 +0530

    Update page.tsx

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,7 +1,32 @@
 import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+import { formatAmount } from '@/lib/utils';
+import TransactionsTable from '@/components/TransactionsTable';
 
-const TransactionHistory = () => {
+const TransactionHistory = async ({ searchParams }: SearchParamProps) => {
+
+  const { id, page } = await searchParams;
+   const currentPage = Number(page as string) || 1;
+   const loggedIn = await getLoggedInUser();
+ 
+   if (!loggedIn) {
+     redirect('/sign-in');
+   }
+ 
+   const accounts = await getAccounts({
+     userId: loggedIn.$id
+   });
+ 
+   if(!accounts) return;
+   
+   const accountsData = accounts?.data;
+   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+ 
+   const account = await getAccount({ appwriteItemId })
+
   return (
     <div className='transactions'>
       <div className='transactions-header'>
@@ -12,7 +37,31 @@ const TransactionHistory = () => {
       </div>
 
       <div className='space-y-6'>
-        <div className='transactions-account'></div>
+        <div className='transactions-account'>
+          <div className='flex flex-col gap-2'>
+            <h2 className='text-18 font-bold text-white'>{account?.data.name}</h2>
+            <p className='text-14 text-blue-25'>{account?.data.officialName}</p>
+            <p className='text-14 font-semibold tracking-[1.1px] text-white'>
+            ●●●● ●●●● ●●●● {account?.data.mask}
+            </p>
+          </div>
+
+          <div className='transactions-account-balance'>
+            <p className='text-14'>Current balance</p>
+            <p className='text-24 text-center font-bold'>{formatAmount(account?.data.currentBalance)}</p>
+          </div>
+        </div>
+
+        <section className="flex w-full flex-col gap-6">
+          <TransactionsTable 
+            transactions={account?.transactions}
+          />
+            {/* {totalPages > 1 && (
+              <div className="my-4 w-full">
+                <Pagination totalPages={totalPages} page={currentPage} />
+              </div>
+            )} */}
+        </section>
       </div>
     </div>
   )
