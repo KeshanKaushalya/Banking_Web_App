4d649bb1faa3eebaf777fd95c9d6ef5e6aa32857
Keshan Kaushalya <madusankakeshan782@gmail.com>
2026-01-05 20:11:37 +0530

    Create PaymentTransferForm.tsx

---
+++ components/PaymentTransferForm.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { createTransfer } from "@/lib/actions/dwolla.actions";
+import { createTransaction } from "@/lib/actions/transaction.actions";
+import { getBank, getBankByAccountId } from "@/lib/actions/user.actions";
+import { decryptId } from "@/lib/utils";
+
+import { BankDropdown } from "./BankDropdown";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
