7cc41a5e0fb91b6ad3309e6d7010f7c17d5089fd
Keshan Kaushalya <madusankakeshan782@gmail.com>
2026-01-05 20:01:14 +0530

    Update page.tsx

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -1,10 +1,46 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import { redirect } from 'next/navigation';
+import { getAccounts } from '@/lib/actions/bank.actions';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
+import BankCard from '@/components/BankCard';
 
-const MyBanks = () => {
+
+const MyBanks = async () => {
+
+    const loggedIn = await getLoggedInUser();
+  
+    if (!loggedIn) {
+      redirect('/sign-in');
+    }
+  
+    const accounts = await getAccounts({
+      userId: loggedIn.$id
+    });
+
   return (
-    <div>
-      Page
-    </div>
+    <section className='flex'>
+      <div className='my-banks'>
+        <HeaderBox 
+          title='My Bank Accounts'
+          subtext='Manage your linked bank accounts securely.'
+        />
+
+        <div className='space-y-4'>
+          <h2 className='header-2'>Your Bank Cards</h2>
+          <div className='flex flex-wrap gap-6'>
+            {accounts && accounts.data.map((a: Account) => 
+            (
+              <BankCard 
+                key={a.id}
+                account={a}
+                userName={loggedIn.firstName || 'User'}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
