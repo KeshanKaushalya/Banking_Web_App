b8d5d8205109c5736772350628f9260f5aecc77d
Keshan Kaushalya <madusankakeshan782@gmail.com>
2026-01-06 22:41:34 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
         <div className="auth-asset">
           <div>
             <Image 
-            src="/icons/Backauth.png"
+            src="/Backauth.png"
             alt="Auth image"
             width={500}
             height={500}
