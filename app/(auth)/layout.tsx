50617c61403b3bfd96e22b57504e6d856f86a0ba
Keshan Kaushalya <madusankakeshan782@gmail.com>
2026-01-06 22:39:04 +0530

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -9,12 +9,12 @@ export default function RootLayout({
     <main className="flex min-h-screen w-full justify-between font-inter">
         {children}
         <div className="auth-asset">
-          <div className="flex justify-left">
+          <div>
             <Image 
             src="/icons/Backauth.png"
             alt="Auth image"
-            width={400}
-            height={400}
+            width={500}
+            height={500}
             />
           </div>
         </div>
