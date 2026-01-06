bc96124eb648769b7633412293a9344a2297bfbe
Keshan Kaushalya <madusankakeshan782@gmail.com>
2026-01-06 18:28:51 +0530

    Update Category.tsx

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Category = () => {
+const Category = ({ category }: CategoryProps) => {
+    
+  
+
   return (
     <div>
-      Top
+      {category.name}
     </div>
   )
 }
